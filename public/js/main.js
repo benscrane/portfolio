57b2ba69cfe3cb32406c947d65c5da2d9041a1c2
ben <bencrane5.12@gmail.com>
2019-03-03 11:21:45 -0500

    Add full screen home section

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(function() {
+
+  // nav position
+  $(document).ready(function() {
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > $(window).height()) {
+        $("#nav").fadeIn(500);
+      } else {
+        $("#nav").fadeOut(500);
+      }
+    });
+  });
+});
