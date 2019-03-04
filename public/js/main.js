a819c7e60002aa343ba7afa7ae6102f09e785d31
ben <bencrane5.12@gmail.com>
2019-03-04 13:37:40 -0500

    Add basic form

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ $(function() {
   // nav position
   $(document).ready(function() {
     $(window).scroll(function() {
-      if ($(this).scrollTop() > $(window).height()) {
+      if ($(this).scrollTop() >= $(window).height()) {
         $("#nav").fadeIn(500);
       } else {
         $("#nav").fadeOut(500);
