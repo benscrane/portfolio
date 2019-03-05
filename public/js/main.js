3b3b24a7dc3aba37e1829a3633d1037a6bce9a85
ben <bencrane5.12@gmail.com>
2019-03-04 20:53:13 -0500

    Add image

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ $(function() {
   // nav position
   $(document).ready(function() {
     $(window).scroll(function() {
-      if ($(this).scrollTop() >= $(window).height()) {
+      if ($(this).scrollTop() >= (0.9 * $(window).height())) {
         $("#nav").fadeIn(400);
       } else {
         $("#nav").fadeOut(400);
